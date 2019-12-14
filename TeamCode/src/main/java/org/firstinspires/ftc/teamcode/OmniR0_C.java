3c8ab64e666fde844e2187bfdcafb0eaac69d822
qwertyuioplkjhgfd <32070@students.ausd.net>
2019-12-14 15:50:03 -0800

    added own version of autonomous r2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniR0_C.java
@@ -0,0 +1,350 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous (
+
+  name  = "2: OmniR0_C"         ,
+  group = "Linear Opmode"
+
+)
+
+// @Disabled
+
+public class OmniR0_C extends LinearOpMode {=
+
+  private ElapsedTime runtime = new ElapsedTime() ;
+
+  private DcMotor         driveNW = null ;
+  private DcMotor         driveNE = null ;
+  private DcMotor         driveSE = null ;
+  private DcMotor         driveSW = null ;
+
+  private DcMotor         slider  = null ;
+  private Servo           grabber = null ;
+  private Servo           dragger = null ;
+
+  private ColorSensor     senseColor   = null ;
+
+  private boolean         red = true;
+
+  double lumin = 800 ;
+
+  private void da ( double power , int time) {
+
+    driveNW.setPower (  power ) ;
+    driveNE.setPower ( -power ) ;
+    driveSE.setPower ( -power ) ;
+    driveSW.setPower (  power ) ;
+
+    sleep(time);
+
+  }
+
+
+  private void db ( double power , int time) {
+
+    driveNW.setPower (  power ) ;
+    driveNE.setPower (  power ) ;
+    driveSE.setPower ( -power ) ;
+    driveSW.setPower ( -power ) ;
+
+    sleep(time);
+
+  }
+
+
+  private void dt ( double power , int time) {
+
+    driveNW.setPower (  power ) ;
+    driveNE.setPower (  power ) ;
+    driveSE.setPower (  power ) ;
+    driveSW.setPower (  power ) ;
+
+  }
+
+
+  private void sliderSpn ( double power ) {
+
+    slider.setPower  (  power ) ;
+
+  }
+
+
+  private void drab () {
+
+    if ( shouldGrab ) {
+
+      grabber.setPosition ( 0.7 ) ;
+
+    } else {
+
+      grabber.setPosition ( 0   ) ;
+
+    }
+
+    if ( shouldDrag ) {
+
+      dragger.setPosition ( 0.7 ) ;
+
+    } else {
+
+      dragger.setPosition ( 0   ) ;
+
+    }
+
+  }
+
+
+  private void update () {
+
+    drab () ;
+
+    time  = getRuntime () * 1000 ;
+
+    distN = ( distN + senseDistN.getDistance ( DistanceUnit.INCH )                 ) / 2 ;
+    distX = ( distX + senseDistX.getDistance ( DistanceUnit.INCH )                 ) / 2 ;
+    distS = ( distS + senseDistS.getDistance ( DistanceUnit.INCH )                 ) / 2 ;
+    lumin = ( lumin + Math.max( senseColor.red () , Math.max ( senseColor.green () , senseColor.blue () ) ) ) / 2 ;
+
+    telemetry.addData ( "distN" , distN ) ;
+    telemetry.addData ( "distX" , distX ) ;
+    telemetry.addData ( "distS" , distS ) ;
+    telemetry.addData ( "lumin" , lumin ) ;
+    telemetry.update  (                 ) ;
+
+  }
+
+
+  private void runWhile ( int option , int comparator ) {
+
+    switch ( option ) {
+
+      case  0 : while ( opModeIsActive() && lumin > comparator ) { update () ; }
+      break ;
+
+      case -1 : while ( opModeIsActive() && distN < comparator ) { update () ; }
+      break ;
+
+      case  1 : while ( opModeIsActive() && distN > comparator ) { update () ; }
+      break ;
+
+      case -2 : while ( opModeIsActive() && distX < comparator ) { update () ; }
+      break ;
+
+      case  2 : while ( opModeIsActive() && distX > comparator ) { update () ; }
+      break ;
+
+      case -3 : while ( opModeIsActive() && distS < comparator ) { update () ; }
+      break ;
+
+      case  3 : while ( opModeIsActive() && distS > comparator ) { update () ; }
+      break ;
+
+      case -4 : while ( opModeIsActive() && time < comparator )  { update () ; }
+      break ;
+
+    }
+
+
+    driveNW.setPower ( 0 ) ;
+    driveNE.setPower ( 0 ) ;
+    driveSE.setPower ( 0 ) ;
+    driveSW.setPower ( 0 ) ;
+    slider.setPower  ( 0 ) ;
+
+
+  }
+
+
+  private void runFor ( int milliseconds ) {
+
+    runWhile ( OPT_TIME_L , milliseconds + (int)time ) ;
+
+  }
+
+
+  @Override
+  public void runOpMode () {
+
+
+
+    driveNW    = hardwareMap.get ( DcMotor.class ,       "driveNW" ) ;
+    driveNE    = hardwareMap.get ( DcMotor.class ,       "driveNE" ) ;
+    driveSE    = hardwareMap.get ( DcMotor.class ,       "driveSE" ) ;
+    driveSW    = hardwareMap.get ( DcMotor.class ,       "driveSW" ) ;
+
+    slider     = hardwareMap.get ( DcMotor.class ,       "slider"  ) ;
+    grabber    = hardwareMap.get ( Servo.class   ,       "grabber" ) ;
+    dragger    = hardwareMap.get ( Servo.class   ,       "dragger" ) ;
+
+    senseColor = hardwareMap.get ( ColorSensor.class ,    "color"   ) ;
+    senseDistN = hardwareMap.get ( DistanceSensor.class , "distN"   ) ;
+    senseDistX = hardwareMap.get ( DistanceSensor.class , "distX"   ) ;
+    senseDistS = hardwareMap.get ( DistanceSensor.class , "distS"   ) ;
+
+
+    waitForStart() ;
+    runtime.reset() ;
+
+
+    // drive sideways to within 5in of depot
+
+    driveX    ( NORMAL_POWER                          ) ;
+    runWhile  ( OPT_DIST_XL , DIST_X_DEPOT_OUTER      ) ;
+
+
+
+    // drive forwards until skystone
+
+    driveY    ( CAUTIOUS_POWER                        ) ;
+    runWhile  ( OPT_LUMIN_G , LUMIN_THRESHOLD         ) ;
+
+
+    // drive back for .2s
+
+    driveY    (-CAUTIOUS_POWER                        ) ;
+    runFor    ( TIME_STONE_MARGIN+500                     ) ;
+
+
+    // drive sideways to stone depot
+
+    driveX    ( FULL_POWER                          ) ;
+    runWhile  ( OPT_DIST_XL , DIST_X_DEPOT_CENTER     ) ;
+
+
+    // drive forward for .2s
+
+    driveY    ( FULL_POWER                        ) ;
+    runFor    ( TIME_STONE_MARGIN                     ) ;
+
+
+    // grab
+
+    shouldGrab = true ;
+
+
+    // wait 1s for grabber to close
+
+    runFor    ( TIME_TOGGLE_DRABBER                   ) ;
+
+
+    // drive sideways to inner track
+
+    driveX    (-NORMAL_POWER                          ) ;
+    runWhile  ( OPT_DIST_XG , DIST_X_INTRACK_OUTER    ) ;
+
+
+    // drive back until centered on foundation's long side
+
+    driveY    (-FULL_POWER                            ) ;
+    runWhile  ( OPT_DIST_SG , DIST_Y_FOUNDATION_CENTER) ;
+
+
+    // drive sideways until touching foundation
+
+    driveX    ( NORMAL_POWER                          ) ;
+    runWhile  ( OPT_DIST_XL , DIST_X_FOUNDATION_OUTER ) ;
+
+
+    driveY    (-NORMAL_POWER);
+    runWhile( OPT_DIST_SG , DIST_Y_WALL);
+
+
+    // deploy foundation grabber
+
+    shouldDrag = true ;
+    runFor    ( TIME_TOGGLE_DRABBER                   ) ;
+
+
+
+    //
+
+    // drive sideways until touching wall
+
+    driveX    (-FULL_POWER                            ) ;
+    runWhile  ( OPT_DIST_XG , DIST_X_WALL             ) ;
+
+
+    // release foundation grabber
+
+    shouldDrag = false ;
+
+    //
+
+
+    // drive forward until clear of foundation
+
+    driveY    ( NORMAL_POWER ) ;
+    runWhile  ( OPT_DIST_SL , DIST_Y_FOUNDATION_OUTER ) ;
+
+
+    // drive sideways until centered on foundation's short side
+
+    driveX    ( NORMAL_POWER ) ;
+    runWhile  ( OPT_DIST_XL , DIST_X_FOUNDATION_CENTER) ;
+
+
+    // turn 180deg
+
+    driveSpn  ( FULL_POWER                            ) ;
+    runFor    ( TIME_ONE_TURN                         ) ;
+
+
+    // raise slider
+
+    sliderSpn ( FULL_POWER                            ) ;
+    runFor    ( TIME_ONE_LEVEL                        ) ;
+
+
+    // drive "forward" until touching foundation's short side
+
+    driveY    ( CAUTIOUS_POWER                        ) ;
+    runFor    ( TIME_FOUNDATION_MARGIN                ) ;
+
+
+    // release
+
+    shouldGrab = false ;
+
+
+    // drive "backward"
+
+    driveY    (-NORMAL_POWER                          ) ;
+    runFor    ( TIME_FOUNDATION_MARGIN                ) ;
+
+    shouldGrab = true ;
+
+
+    // lower slider
+
+    sliderSpn (-FULL_POWER                            ) ;
+    runFor    ( TIME_ONE_LEVEL                        ) ;
+
+
+    // turn 180deg
+
+    driveSpn  (-FULL_POWER                            ) ;
+    runFor    ( TIME_ONE_TURN                         ) ;
+
+
+    // drive sideways to inner track
+
+    driveX    (-NORMAL_POWER                          ) ;
+    runWhile  ( OPT_DIST_XG , DIST_X_INTRACK_OUTER    ) ;
+
+
+    // drive forward to under bridge
+
+    driveY    ( FULL_POWER                            ) ;
+    runWhile  ( OPT_DIST_SL , DIST_Y_SKYBRIDGE        ) ;
+  }
+}
