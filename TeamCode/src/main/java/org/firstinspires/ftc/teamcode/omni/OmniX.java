97fa05763c088094d9cf278a5bd3f1aa5f292d16
LXing <31298876+FlyOrBoom@users.noreply.github.com>
2020-03-01 15:13:11 -0800

    Rewrote OmniX
    - snake_case all the things
    - removed redundancies
    >- functionalized duplicate equations
    >- removed folders
    - more logical
    >- on servos, a state of 1 always means active, a state of 0 always means retracted
    >- for telemetry, unicode arrows represent vectors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/omni/OmniX.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/omni/OmniX.java
@@ -1,130 +1,110 @@
-package org.firstinspires.ftc.teamcode.omni;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
-@TeleOp (
-  name  = "OmniX"         ,
-  group = "2"
-)
-//@Disabled
+package org.firstinspires.ftc.teamcode.omni ;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode ;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp ;
+import com.qualcomm.robotcore.hardware.CRServo ;
+import com.qualcomm.robotcore.hardware.DcMotor ;
+import com.qualcomm.robotcore.hardware.Servo ;
+
+@TeleOp ( name = "OmniX" )
+
 public class OmniX extends LinearOpMode {
-
-  private DcMotor driveNW = null;
-  private DcMotor driveNE = null;
-  private DcMotor driveSE = null;
-  private DcMotor driveSW = null;
-
-  private DcMotor slider = null;
-  private Servo grabber = null;
-  private Servo dragger = null;
-  private CRServo dropper = null;
-
-  double left_stick_right = 0 ;
-  double right_stick_right = 0 ;
-  double left_stick_forward = 0 ;
-  double right_stick_forward = 0 ;
-  double trigger_left = 0 ;
-  double trigger_right = 0 ;
-  double bumper_left = 0 ;
-  double bumper_right = 0 ;
-
-  // minimum throttle for motors to have sufficient grip
-
-  double drive_right  = 0 ;
-  double drive_forward  = 0 ;
-  double drive_clockwise    = 0 ;
-
-  double slider_power = 0   ;
-  double grabber_position  = 0.3 ;
-  double dragger_position  = 0.2 ;
-  double dropper_position  = 0 ;
-
-  private int boolean_to_int( boolean bool ){
-	return bool ? 1 : -1;
+  
+  private double diffbool ( boolean first , boolean second ) {
+    return ( first ? 1 : 0 ) - ( second ? 1 : 0 ) ;
   }
-
-	private int clamp ( int low , int var , int high ){
-		return Math.max(low,Math.min(var,high));
-	}
-	private double clamp ( int low , double var , int high ){
-		return Math.max(low,Math.min(var,high));
-	}
+  
+  private double curve ( double left_stick , double right_stick ) {
+    return ( Math.pow ( left_stick , 3 ) + Math.pow ( right_stick, 3 ) ) / 2 ;
+  }
+  
+  private double clamp ( double min , double input, double max ) {
+    return Math.max ( min , Math.min ( input , max ) ) ;
+  }
 
   @Override
   public void runOpMode() {
+  
+		telemetry.addLine ( "OmniX INITIALIZING" ) ;
+		telemetry.update () ;
+  
+		private DcMotor motor_nw = hardwareMap.get ( DcMotor.class , "driveNW" ) ;
+		private DcMotor motor_ne = hardwareMap.get ( DcMotor.class , "driveNE" ) ;
+		private DcMotor motor_se = hardwareMap.get ( DcMotor.class , "driveSE" ) ;
+		private DcMotor motor_sw = hardwareMap.get ( DcMotor.class , "driveSW" ) ;
+  
+		private DcMotor slider = hardwareMap.get ( DcMotor.class , "slider" ) ;
+		private Servo grabber = hardwareMap.get ( Servo.class , "grabber" ) ;
+		private Servo dragger = hardwareMap.get ( Servo.class , "dragger" ) ;
+		private CRServo dropper = hardwareMap.get ( CRServo.class , "dropper" ) ;
+  
+		double drive_right = 0 ;
+		double drive_forward = 0 ;
+		double drive_clockwise = 0 ;
 
-	telemetry.addData ( "Status" , "OmniX Initialized" ) ;
-	telemetry.update () ;
+    double grabber_state = 1 ;
+    double dragger_state = 0 ;
 
-	driveNW = hardwareMap.get ( DcMotor.class , "driveNW" ) ;
-	driveNE = hardwareMap.get ( DcMotor.class , "driveNE" ) ;
-	driveSE = hardwareMap.get ( DcMotor.class , "driveSE" ) ;
-	driveSW = hardwareMap.get ( DcMotor.class , "driveSW" ) ;
-
-	slider  = hardwareMap.get ( DcMotor.class , "slider"  ) ;
-	grabber = hardwareMap.get ( Servo.class   , "grabber" ) ;
-  dragger = hardwareMap.get ( Servo.class   , "dragger" ) ;
-  dropper = hardwareMap.get ( CRServo.class   , "dropper" ) ;
-
-	waitForStart ( ) ;
-	telemetry.addData ( "Status" , "OmniX Active" ) ;
-
-	while ( opModeIsActive () ) {
-
-	  //driving define
-
-	  left_stick_right = gamepad1.left_stick_x  + gamepad2.left_stick_x  ;
-	  right_stick_right = gamepad1.right_stick_x + gamepad2.right_stick_x ;
-
-	  left_stick_forward = gamepad1.left_stick_y  + gamepad2.left_stick_y  ;
-	  right_stick_forward = gamepad1.right_stick_y + gamepad2.right_stick_y ;
-
-	  trigger_left = gamepad1.left_trigger  + gamepad2.left_trigger  ;
-	  trigger_right = gamepad1.right_trigger + gamepad2.right_trigger ;
-	  bumper_left = boolean_to_int( gamepad1.left_bumper  ) + boolean_to_int( gamepad2.left_bumper  );
-	  bumper_right = boolean_to_int( gamepad1.right_bumper ) + boolean_to_int( gamepad2.right_bumper );
-
-	  drive_forward = ( Math.pow ( left_stick_forward , 3 ) + Math.pow ( right_stick_forward , 3 ) ) / 2 ;
-	  drive_right = - ( Math.pow ( left_stick_right , 3 ) + Math.pow ( right_stick_right , 3 ) ) / 2 ;
-	  drive_clockwise = ( trigger_left - trigger_right ) + 0.1 * ( bumper_left - bumper_right ) ;
-
-	  driveNW.setPower (   drive_right + drive_forward + drive_clockwise ) ;
-	  driveNE.setPower (   drive_right - drive_forward + drive_clockwise ) ;
-	  driveSE.setPower ( - drive_right - drive_forward + drive_clockwise ) ;
-	  driveSW.setPower ( - drive_right + drive_forward + drive_clockwise ) ;
-
-	  slider.setPower ( (gamepad1.dpad_left||gamepad2.dpad_up) ? 1 : (gamepad1.dpad_right||gamepad2.dpad_down) ? -1 : 0 ) ;
-    if ( gamepad1.dpad_up || gamepad2.dpad_left ){
-      grabber_position += 0.002;
-    } else if (gamepad1.dpad_down || gamepad2.dpad_right) {
-      grabber_position = 0;
+    double dropper_power = 0 ;
+    double slider_power = 0 ;
+  
+  	telemetry.addLine ( "OmniX INITIALIZED" ) ;
+  	telemetry.update () ;
+  	
+  	waitForStart () ;
+  	
+  	telemetry.addLine ( "OmniX ACTIVE" ) ;
+  	telemtry.update() ;
+  
+  	while ( opModeIsActive () ) {
+  
+      drive_forward = curve (
+        gamepad1.left_stick_y + gamepad2.left_stick_y ,
+        gamepad1.right_stick_y + gamepad2.right_stick_y
+      );
+  
+      drive_right = - curve (
+        gamepad1.left_stick_x + gamepad2.left_stick_x ,
+        gamepad1.right_stick_x - gamepad2.right_stick_x
+      ) ;
+  
+      drive_clockwise = (
+          gamepad1.left_trigger + gamepad2.left_trigger
+        - gamepad1.right_trigger - gamepad2.right_trigger
+        + diffbool ( gamepad1.left_bumper || gamepad2.left_bumper , gamepad1.right_bumper || gamepad2.right_bumper ) / 9
+      ) ;
+  
+      motor_nw.setPower ( drive_right + drive_forward + drive_clockwise ) ;
+      motor_ne.setPower ( drive_right - drive_forward + drive_clockwise ) ;
+      motor_se.setPower ( - drive_right - drive_forward + drive_clockwise ) ;
+      motor_sw.setPower ( - drive_right + drive_forward + drive_clockwise ) ;
+  
+      if ( gamepad1.dpad_up || gamepad2.dpad_left ){
+        grabber_state = clamp ( 0 , grabber_state - 0.003 , 0.9 ) ;
+      } else if ( gamepad1.dpad_down || gamepad2.dpad_right ) {
+        grabber_state = 1 ;
+      }
+  
+      if ( gamepad1.y || gamepad2.y ) {
+        dragger_state = 0 ;
+      } else if ( gamepad1.x || gamepad2.x ) {
+        dragger_state = 1 ;
+      }
+      
+      grabber.setPosition ( 1 - grabber_state * 0.75 - 0.1 ) ;
+      dragger.setPosition ( dragger_state * 0.35 + 0.5 ) ;
+  
+      dropper.setPower ( diffbool ( gamepad1.a || gamepad2.a , gamepad1.b || gamepad2.b ) ) ;
+      slider.setPower ( diffbool ( gamepad1.dpad_left || gamepad2.dpad_up , gamepad1.dpad_right || gamepad2.dpad_down ) ) ;
+  
+      telemetry.addLine ( "OmniX RUNNING" ) ;
+      telemetry.addLine ( "→" + drive_right ) ;
+      telemetry.addLine ( "↑" + drive_forward ) ;
+      telemetry.addLine ( "↻" + drive_clockwise ) ;
+      telemetry.addLine ( "s" + slider_power ) ;
+      telemetry.addLine ( "g" + grabber_state ) ;
+      telemetry.addLine ( "d" + dragger_state ) ;
+      telemetry.update () ;
+  
     }
-    grabber_position = Math.max(0.15,Math.min(grabber_position,0.9));
-    if( gamepad1.y||gamepad2.y||gamepad1.a||gamepad2.a ){
-      dragger_position = ( gamepad1.y || gamepad2.y ) ? 0.2 : 0.85 ;
-    }
-    if( gamepad1.x||gamepad2.x||gamepad1.b||gamepad2.b ) {
-      dropper_position = ( gamepad1.x || gamepad2.x ) ? 0.5 : 0.6 ;
-    }
-    grabber.setPosition(grabber_position);
-    dragger.setPosition(dragger_position);
-    dropper.setPower( (gamepad1.x||gamepad2.x) ? 1 : (gamepad1.b||gamepad2.b) ? -1 : 0);
-
-	  // telemetry
-
-	  telemetry.addData ( "Status" , "OmniX Running" ) ;
-	  telemetry.addData ( "drive right" , drive_right ) ;
-	  telemetry.addData ( "drive forward" , drive_forward ) ;
-	  telemetry.addData ( "drive clockwise" , drive_clockwise ) ;
-	  telemetry.addData ( "slider power" , slider_power ) ;
-	  telemetry.addData ( "grabber position" , grabber_position ) ;
-	  telemetry.addData ( "dragger position" , dragger_position ) ;
-	  telemetry.update () ;
-
-	}
   }
 }
